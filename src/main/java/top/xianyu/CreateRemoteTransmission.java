e0d7e6e0b75277d7433b205e327c7eec308b9657
Create-Remote-Transmission <763875579@qq.com>
2025-04-01 20:08:23 +0800

    Initial commit

---
+++ src/main/java/top/xianyu/CreateRemoteTransmission.java
@@ -0,0 +1,24 @@
+package top.xianyu;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CreateRemoteTransmission implements ModInitializer {
+	public static final String MOD_ID = "create-remote-transmission";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
